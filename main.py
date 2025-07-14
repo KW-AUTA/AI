1cc2f15363898c1984ba68d69ae1070829a84a9b
5jun99 <oj041088@gmail.com>
2025-07-14 23:23:22 +0000

    feat: ui 테스트 llm 기능 구현

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
-from routes import test_route
-import logging
+from routes import test_route, evaluate
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 app.include_router(test_route.router)
+app.include_router(evaluate.router)
 
+app.mount("/static", StaticFiles(directory="static"), name="static")
 @app.get("/")
 async def root():
     return {"message": "Welcome to the API"}
---
+++ routes/evaluate.py
@@ -0,0 +1,9 @@
+from fastapi import APIRouter
+from routes.dto.request import UITestRequest
+from service.evaluator import evaluate_all_frames_logic
+
+router = APIRouter()
+
+@router.post("/evaluate-ui-with-highlight")
+async def evaluate_ui_with_highlight(request: UITestRequest):
+    return await evaluate_all_frames_logic(request.figmaJsonUrl)
